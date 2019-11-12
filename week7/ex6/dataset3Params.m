36cf3aebc8ee34e265a684946f9fca0b93aca6ed
Firas <firasaldabbas+git@gmail.com>
2019-11-11 21:03:05 -0500

    week 7; 2 / 4; dataset3Params
    So goddamn infuriating

--- week7/ex6/dataset3Params.m
+++ week7/ex6/dataset3Params.m
@@ -22,10 +22,29 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+range = [0.01 0.03 0.1 0.3 1 3 10 30];
+results = ones(64, 3);
+curr_row = 0;
 
+for curr_C = range
+  for curr_sigma = range
+    curr_row += 1;
+    disp(curr_row)
+    %svmTrain(X, Y, C, kernelFunction, ...
+     %                       tol, max_passes)
+    %function sim = gaussianKernel(x1, x2, sigma)
+    model = svmTrain(X, y, curr_C, @(x1, x2) gaussianKernel(x1, x2, curr_sigma));
+    predictions = svmPredict(model, Xval);
+    prediction_error = mean(double(predictions ~= yval));
+    
+    results(curr_row, :) = [curr_C curr_sigma prediction_error];
+  end
+end
 
-
-
+final = sortrows(results, 3)(1, :); % For some reason using min fucked everything up, I am so mad
+C = final(1)
+sigma = final(2)
+  
 
 
 
